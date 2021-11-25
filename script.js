793064ba0afe5fb9c260c111485d7b50a3abc2e3
faaabi93 <fabian.baiersdoerfer@gmx.de>
2021-11-25 13:53:43 +0100

    added new functionality

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
 const container = document.querySelector(".container");
+const btnClear = document.querySelector(".btn-clear");
+const btnColor = document.querySelector(".btn-color");
+const btnBlack = document.querySelector(".btn-black");
+const btnResize = document.querySelector(".btn-resize");
+let currentMode = "color";
 
+btnClear.addEventListener("click", clear);
+btnBlack.addEventListener("click", blackBtn);
+btnColor.addEventListener("click", colorBtn);
+btnResize.addEventListener("click", resizeBtn);
+
+function resizeBtn() {
+    let newSize = window.prompt("Test");
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createDivs(newSize);
+    const cells = document.querySelectorAll(".item");
+    cells.forEach(cell => cell.addEventListener("mouseover", addColor))
+};
+
+function addContainerTemplate(n) {
+    container.style.gridTemplateColumns = `repeat(${n}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${n}, 1fr)`
+}
 
 function addColor(e) {
     if(e.target.style.backgroundColor) return;
     e.target.style.backgroundColor = `rgb(${getRandomColor()})`;
+};
+
+function addBlack(e) {
+    if(e.target.style.backgroundColor) return;
+    e.target.style.backgroundColor = "rgb(0,0,0)"
+};
+
+function colorBtn() {
+    const cells = document.querySelectorAll(".item");
+    cells.forEach(function(cell) {
+        clear();
+        cell.removeEventListener("mouseover", addBlack);
+        cell.addEventListener("mouseover", addColor);  
+        currentMode = "color";  
+    });
+};
+
+function blackBtn() {
+    const cells = document.querySelectorAll(".item");
+    cells.forEach(function(cell) {
+        clear();
+        cell.removeEventListener("mouseover", addColor);
+        cell.addEventListener("mouseover", addBlack);
+        currentMode = "black";
+    });
+};
+
+function clear() {
+    const cells = document.querySelectorAll(".item");
+    cells.forEach(cell => cell.style.backgroundColor = "")
 };
 
 function getRandomColor() {
@@ -15,14 +69,15 @@ function getRandomInt(max) {
 };
 
 function createDivs(n) {
-    for(let i = 0; i <= n; i++) {
+    for(let i = 0; i < n*n; i++) {
         let newDiv = document.createElement("div")
         newDiv.classList.add("item");
         container.appendChild(newDiv)
     };
-
     const cells = document.querySelectorAll(".item");
+    addContainerTemplate(n);
     cells.forEach(cell => cell.addEventListener("mouseover", addColor));
 };
+
 
 createDivs(16)
